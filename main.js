2e0d3f03f2ace0bad3933d02d8f3621fbacf8dcf
josh <jlcastro@ucsd.edu>
2019-12-07 12:32:23 -0800

    fixed line and timeline

--- main.js
+++ main.js
@@ -5,89 +5,6 @@ const JSONFileName = 'https://raw.githubusercontent.com/castrojoshua18/dsc106_fi
 
 var fullData;
 
-['mouseleave'].forEach(function (eventType) {
-    document.getElementById('syncedGrid').addEventListener(
-        eventType,
-        function (e) {
-            var chart,
-                point,
-                i,
-                event;
-            
-                for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-                    chart = Highcharts.charts[i];
-                    event = chart.pointer.normalize(e);
-                    point = chart.series[0].searchPoint(event, true);
-                    
-                    if (point) {
-                        point.onMouseOut(); 
-                        chart.tooltip.hide(point);
-                        chart.xAxis[0].hideCrosshair(); 
-                    }
-                }
-            }
-        )
-    });
-    
-['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
-document.getElementById('syncedGrid').addEventListener(
-    eventType,
-    function (e) {
-        var chart,
-            point,
-            i,
-            event
-
-        for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-            chart = Highcharts.charts[i];
-            // Find coordinates within the chart
-            event = chart.pointer.normalize(e);
-            // Get the hovered point
-            point = chart.series[0].searchPoint(event, true);
-
-            if (point) {
-                point.highlight(e);
-            }
-        }
-    }
-);
-});
-
-/**
-* Highlight a point by showing tooltip, setting hover state and draw crosshair
-*/
-Highcharts.Point.prototype.highlight = function (event) {
-    event = this.series.chart.pointer.normalize(event);
-    this.onMouseOver(); // Show the hover marker
-    this.series.chart.tooltip.refresh(this); // Show the tooltip
-    this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-    this.series.chart.yAxis[0].drawCrosshair(event, this);
-};
-
-
-/**
-* Synchronize zooming through the setExtremes event handler.
-*/
-function syncExtremes(e) {
-var thisChart = this.chart;
-
-if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
-    Highcharts.each(Highcharts.charts, function (chart) {
-        if (chart !== thisChart) {
-            if (chart.xAxis[0].setExtremes) { // It is null while updating
-                chart.xAxis[0].setExtremes(
-                    e.min,
-                    e.max,
-                    undefined,
-                    false,
-                    { trigger: 'syncExtremes' }
-                );
-            }
-        }
-    });
-    }
-}
-
 Highcharts.ajax({
     url: JSONFileName,
     dataType: 'text',
@@ -252,11 +169,6 @@ Highcharts.ajax({
                 text: "timeline"
             },
             tooltip: {
-                crosshairs: [{
-                    width: 2,
-                    color: 'red',
-                    zIndex: 3
-                    }],
                 positioner : function () {
                     return {x : this.chart.chartWidth - this.label.width-20 , y : 10}
                 },
@@ -372,8 +284,8 @@ Highcharts.ajax({
                     },
                     {
                         x: Date.UTC(2019,3,10),
-                        name: "Team Accomplishment",
-                        label: "Team",
+                        name: "End of Season",
+                        label: "End",
                         description: "Finished season with 60-22 record. First time since 1980-1981 season"
                     }
                 ]
